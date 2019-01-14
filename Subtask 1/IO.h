6a284fb3c060132b729b3438d7740fc97cd31dc1
Rajat Jaiswal <cs5170415@cse.iitd.ac.in>
2019-01-15 00:57:31 +0530

    combined header and cpp files

--- Subtask 1/IO.h
+++ Subtask 1/IO.h
@@ -1,13 +1,80 @@
-#include <vector>
-#include <fstream>
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 #ifndef IO_H
 #define IO_H
-    vector<vector<float>> inputMatrix(string filename);
-    vector<float> inputVector(string filename);
+    // INPUT
+    /* Input a matrix from a file.
+    The matrix is assumed to be in column major order.
+    Number of rows is not a predetermined number. */
+    vector<vector<float>> inputMatrix(string filename) {
+        vector<vector<float>> matrix;
+        try {
+            ifstream input(filename);
+            string line;
+            getline(input, line);
+            float val{};
+            istringstream s(line);
+            while(s >> val)
+                matrix.push_back(vector<float>{val});
+            int matrixSize = matrix.size();
+            for(int i=1; i<matrixSize; i++) {
+                getline(input, line);
+                istringstream s(line);
+                for(int j=0; j<matrixSize; j++) {
+                    s >> val;
+                    matrix[j].push_back(val);
+                }
+            }
+        }
+        catch(...) {
 
-    void writeSquareMatrix(vector<vector<float>> c, ostream& out=cout);
-    void writeVector(vector<float> c, ostream &out = cout);
+        }
+        return matrix;
+    }
+
+    /* Input a vector from a file.
+        The vector is assumed to be in a single line.
+        Size is not known.
+    */
+    vector<float> inputVector(string filename) {
+        vector<float> vctr{};
+        try {
+            ifstream input(filename);
+            string line;
+            float val{};
+            getline (input, line);
+            istringstream s(line);
+            while (s >> val) {
+                vctr.push_back(val);
+            }
+        }
+        catch(...) {
+            cout << "Some Problem [File I/O] [Emptry Matrix Returned]" << "\n";
+        }
+        return vctr;
+    }
+
+
+    // OUTPUT
+    /* Print/write a 2D square matrix */
+    void writeSquareMatrix(vector<vector<float>> c, ostream& out=cout) {
+        for(int i=0; i<c.size(); i++) {
+                for(int j=0; j<c[i].size(); j++)
+                        out << c[i][j] << setw(10) << " ";
+                out << "\n";
+        }
+    }
+
+    /* Print/write a 1D vector */
+    void writeVector(vector<float> c, ostream &out = cout) {
+        for(int i=0; i<c.size(); i++)
+                out << c[i] << " ";
+        out << "\n";
+    }
+    
 #endif
