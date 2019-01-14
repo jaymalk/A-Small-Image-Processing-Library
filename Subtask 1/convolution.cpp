901c2b6f6fb84c3fc15f88058498f74d80d9d356
Rajbir Malik <cs1170416@iitd.ac.in>
2019-01-14 22:21:38 +0530

    Merge convolution

--- Subtask 1/convolution.cpp
+++ Subtask 1/convolution.cpp
@@ -1,10 +1,64 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <cmath>
 #include <algorithm>
 #include "./padding.cpp"
 using namespace std;
 
+// FORMATTING FUNCTIONS
+/* Change 2D square matrix to 1D matrix(row major) */
+vector<float> changeTo1D(vector<vector<float>> kernel) {
+        vector<float> kernel1D(kernel[0].begin(), kernel[0].end());
+        for(int i=1; i<kernel.size(); i++)
+                kernel1D.insert(kernel1D.end(), kernel[i].begin(), kernel[i].end());
+        return kernel1D;
+}
+
+/* Change 1D matrix (in row major form) (assuming size to be perfect square) to 2D square matrix*/
+vector<vector<float>> changeToSqaure2D(vector<float> col) {
+        vector<vector<float>> matrix;
+        int size = (int)(sqrt(col.size()));
+        for(int i=0; i<size; i++)
+                matrix.push_back(vector<float>(col.begin()+i*size, col.begin()+(i+1)*size));
+        return matrix;
+}
+
+// DIRECT CONVOLUTION
+
+vector<vector<float>> invertKernel(vector<vector<float>>& kernel) {
+    vector<float> kernel1D = changeTo1D(kernel);
+    reverse(kernel1D.begin(), kernel1D.end());
+    kernel = changeToSqaure2D(kernel1D);
+    return kernel;
+}
+
+vector<vector<float>> mapping(vector<vector<float>> kernel, vector<vector<float>> matrix, bool convolution, bool padding=false) {
+    if(padding)
+        matrix = pad(matrix, kernel.size()-1);
+    if(convolution)
+        kernel = invertKernel(kernel);
+
+    int kernelSize = kernel.size(), matrixSize = matrix.size();
+
+    vector<vector<float>> conv{};
+
+    for(int i=0; i<matrixSize-kernelSize+1; i++)
+        conv.push_back(vector<float>{});
+
+    for(int i=0; i<matrixSize-kernelSize+1; i++)
+        for(int j=0; j<matrixSize-kernelSize+1; j++) {
+            float val = 0.0;
+            for(int k=0; k<kernelSize; k++)
+                for(int l=0; l<kernelSize; l++)
+                    val += matrix[i+k][j+l]*kernel[k][l];
+            conv[i].push_back(val);
+        }
+
+    return conv;
+}
+
+// CONVOLUTION BY MULTIPLICATION
 
 /* Convert the matrix to toeplitz matrix, by using the order of square kernel
    The conversion of 2D mapping is converted to 1D considering row major*/
@@ -33,23 +87,6 @@ vector<float> getConvolutionByMultiplication(vector<vector<float>> toeplitz, vec
         return product;
 }
 
-/* Change 2D square matrix to 1D matrix(row major) */
-vector<float> changeTo1D(vector<vector<float>> kernel) {
-        vector<float> kernel1D(kernel[0].begin(), kernel[0].end());
-        for(int i=1; i<kernel.size(); i++)
-                kernel1D.insert(kernel1D.end(), kernel[i].begin(), kernel[i].end());
-        return kernel1D;
-}
-
-/* Change 1D matrix (in row major form) (assuming size to be perfect square) to 2D square matrix*/
-vector<vector<float>> changeToSqaure2D(vector<float> col) {
-        vector<vector<float>> matrix;
-        int size = (int)(sqrt(col.size()));
-        for(int i=0; i<size; i++)
-                matrix.push_back(vector<float>(col.begin()+i*size, col.begin()+(i+1)*size));
-        return matrix;
-}
-
 /* The encapsulating layer for complete function
  First two parameters are kernel and matrix, resp.
  Third for selection between convolution (true) and cross-correlation (false)
@@ -65,21 +102,6 @@ vector<vector<float>> convolutionByMultiplication(vector<vector<float>> kernel, 
         return changeToSqaure2D(conv1d);
 }
 
-/* Print a 2D square matrix */
-void print2DSquareMatrix(vector<vector<float>> c) {
-        for(int i=0; i<c.size(); i++) {
-                for(int j=0; j<c[i].size(); j++)
-                        printf("%5.2f ", c[i][j]);
-                cout << "\n";
-        }
-}
-
-/* Print a 1D matrix */
-void print1DMatrix(vector<float> c) {
-        for(int i=0; i<c.size(); i++)
-                cout << c[i] << " ";
-        cout << "\n";
-}
 
 /*
 int main() {
@@ -107,6 +129,11 @@ int main() {
   // Check for cross-correlation with padding
   c = convolutionByMultiplication(a, b, false, true);
   print2DSquareMatrix(c);
+
+  // Direct Functions Check
+  print2DSquareMatrix(mapping(a, b, false));
+  print2DSquareMatrix(mapping(a, b, true));
+  print2DSquareMatrix(mapping(a, b, false, true));
   return 0;
 }
 */
